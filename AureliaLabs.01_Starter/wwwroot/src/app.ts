9eadb1852c50a8c8b2d2441d505fd4ae04ea59aa
Bojan Veljanovski <bojanv91@live.com>
2015-02-07 00:48:56 +0100

    Added new page like a new page;

--- AureliaLabs.01_Starter/wwwroot/src/app.ts
+++ AureliaLabs.01_Starter/wwwroot/src/app.ts
@@ -3,19 +3,18 @@ import AureliaRouter = require("aurelia-router");
 
 export class App {
 
+    //array of types to be injected in the constructor
     static inject = [AureliaRouter.Router];
 
-    constructor(private router: AureliaRouter.Router) {
+    //the router MUST be named router, because it is a router
+    constructor(public router: AureliaRouter.Router) {
         this.router.configure(config => {
-            config.title = 'Aurelia';
+            config.title = "Aurelia";
             config.map([
-                {
-                    route: ['', 'welcome'],
-                    moduleId: 'welcome',
-                    nav: true,
-                    title: 'Welcome'
-                }
+                { route: ["", "welcome"], nav: true, moduleId: "welcome", title: "Welcome" },
+                { route: "flickr", nav: true, moduleId: "flickr", title: "Flickr view" }
             ]);
+
         });
-    } 
+    }
 }

e36995df14ffa1f087805918881ede9232fe1896
Bojan Veljanovski <bojanv91@live.com>
2015-02-07 03:59:56 +0100

    Small refactorings

--- AureliaLabs.01_Starter/wwwroot/src/app.ts
+++ AureliaLabs.01_Starter/wwwroot/src/app.ts
@@ -1,20 +1,19 @@
 ﻿/// <reference path="../typings/aurelia/aurelia.d.ts" />
-import AureliaRouter = require("aurelia-router");
+import AurRouter = require("aurelia-router");
 
 export class App {
 
     //array of types to be injected in the constructor
-    static inject = [AureliaRouter.Router];
+    static inject = [AurRouter.Router];
 
     //the router MUST be named router, because it is a router
-    constructor(public router: AureliaRouter.Router) {
+    constructor(public router: AurRouter.Router) {
         this.router.configure(config => {
             config.title = "Aurelia";
             config.map([
-                { route: ["", "welcome"], nav: true, moduleId: "welcome", title: "Welcome" },
-                { route: "flickr", nav: true, moduleId: "flickr", title: "Flickr view" }
+                { route: ["", "welcome"], moduleId: "welcome", nav: true, title: "Welcome" },
+                { route: "flickr", moduleId: "flickr", nav: true, title: "Flickr view" }
             ]);
-
         });
     }
 }

424e3c23f36607eefdf24ba7b0ad4cb02b3141b9
Bojan Veljanovski <bojanv91@live.com>
2015-05-16 19:34:30 +0200

    Added typings as found on https://github.com/cmichaelgraham/aurelia-typescript-atom/tree/900655787fc7775d87e79e061eb1539415b3b856/skel-nav-ts/typings/aurelia

---
+++ AureliaLabs.01_StarterSEDC2/client/src/app.ts
@@ -0,0 +1,18 @@
+import "bootstrap";
+import "bootstrap/css/bootstrap.css!";
+
+export class App {
+    
+    //constructor() {
+    configureRouter(config, router){
+        config.title = "Aurelia";
+        config.map([
+          { route: ["","welcome"],  moduleId: "./welcome",      nav: true, title:"Welcome" },
+          { route: "flickr",        moduleId: "./flickr",       nav: true, title:"Flickr" },
+          { route: "child-router",  moduleId: "./child-router", nav: true, title:"Child Router" }
+        ]);
+
+        this.router = router;
+    }
+    //}
+}
